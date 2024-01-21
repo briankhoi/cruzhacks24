f07564d6fe0d248fbc109321b5c50875cba4f852
Brian <86499417+briankhoi@users.noreply.github.com>
2024-01-20 22:44:28 -0800

    changed to transparent icon

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -10,7 +10,7 @@ export default function Navbar({user, onLogout}) {
    <div class="navbar">
      <nav className="navbar navbar-light">
        <NavLink className="navbar-brand" to="/">
-       <img alt="" class="logo" src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png"></img>
+       <img alt="logo" class="logo" src="/icon_transparent.png"></img>
        </NavLink>
      </nav>
       <Dropdown
